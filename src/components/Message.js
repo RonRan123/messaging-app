c7bb41361ed75233753864e685df020abbbd5d47
RonRan123 <ronith.ranjan123@gmail.com>
2022-05-30 10:51:58 -0400

    formatted date in messages

--- src/components/Message.js
+++ src/components/Message.js
@@ -3,9 +3,13 @@ import React from 'react'
 function Message(props) {
   const {username, text, createdAt} = props.msg;
   console.log(props);
+  let date = new Date(null);
+  date.setSeconds(createdAt._seconds);
+//   let time = `${date.getMonth() +1 }/${date.getDate()} ${date.getHours()}:${date.getMinutes()}`;
+  let time = date.toLocaleDateString('en-us', { weekday:"short", year:"numeric", month:"short", day:"numeric", hour:"numeric", minute:"numeric"}) 
   return (
     <div>
-        <h3>Message from {username} at </h3>
+        <h3>Message from {username} at {time}</h3>
         <p>{text}</p>
     </div>
   )
